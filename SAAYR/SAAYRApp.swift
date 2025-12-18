a9fb8e8d7496d0b4a494c52aec93efc1f9e05f8b
awaisraza615 <awaisraza615@gmail.com>
2025-12-19 02:07:12 +0500

    Initial Commit

---
+++ SAAYR/SAAYRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAAYRApp.swift
+//  SAAYR
+//
+//  Created by Awais Raza on 19/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SAAYRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
